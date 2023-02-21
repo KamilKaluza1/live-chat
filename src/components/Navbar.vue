4225d33a519afb17e06d2a29fab240b62647759b
Kamil Kałuża <kamil321.94@gmail.com>
2023-02-21 14:00:24 +0100

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+    <nav>
+        <div>
+            <p>Hello there displayName </p>
+            <p class="email">  email </p>
+        </div>
+        <button @click="handleClick">Logout</button>
+    </nav>
+</template>
+
+<script>
+import useLogout from '../composables/useLogout'
+    export default{
+        setup() {
+            const {error, logout} = useLogout()
+
+
+            const handleClick = async() => {
+                await logout()
+                if(!error.value){
+                    console.log("user logout ")
+                }
+
+            }
+            return {handleClick}
+        }
+    }
+
+
+</script>
+
+<style>
+nav{
+    padding: 20px;
+    border-bottom: solid 1px #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+nav p {
+    margin: 2px auto;
+    font-size: 14px;
+    color: #444;
+}
+
+p.email{
+    font-size: 14px;
+    color: #999;
+}
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,4 +1,15 @@
 <template>
+    <div class="container">
+        <Navbar/>
+    </div>
 <h2>chatroom</h2>
 </template>
 
+<script>
+import Navbar from '../components/Navbar.vue'
+export default{
+    components: {Navbar}
+}
+
+
+</script>
