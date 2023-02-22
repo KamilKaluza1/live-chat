1c31190c221997950b3eef2881cecf89ff43f48b
Kamil Kałuża <kamil321.94@gmail.com>
2023-02-22 15:04:38 +0100

    chat

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container">
         <Navbar/>
+        <ChatWindow/>
+        <NewChatForm/>
     </div>
 <h2>chatroom</h2>
 </template>
@@ -8,11 +10,13 @@
 <script>
 
 import getUser from '@/composables/getUser';
-import { projectAuth } from '@/firebase/config';
+import {watch} from 'vue'
 import { useRouter } from 'vue-router';
 import Navbar from '../components/Navbar.vue'
+import NewChatForm from '../components/NewChatForm.vue'
+import ChatWindow from '../components/ChatWindow.vue'
 export default{
-    components: {Navbar},
+    components: {Navbar, NewChatForm, ChatWindow},
 
 
     setup(){
